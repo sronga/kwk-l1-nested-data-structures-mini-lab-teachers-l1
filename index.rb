74120d426fe52a7b4cfc4da41f03bce61eca4531
sronga <sr899@georgetown.edu>
2018-07-21 14:42:07 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -6,28 +6,34 @@ ingredients = [
     ["mayo", "mustard", "tabasco"]
 ]
 
-def sandwich_request(ingredients, order) 
-  # your code here!
+def sandwich_request(ingredients, order)
+
+order.each do |eachitem|
+  ingredients.each do |eachingred|
+    if eachingred.include?(eachitem) return true
+    end
+    return false
+  end
 end
 
 # example city_info hash
 cities = {
-  :new_york => { 
+  :new_york => {
     :mayor => "Bill DeBlasio",
     :population => 8337000,
     :website => "http://www.nyc.gov"
   },
-  :los_angeles => { 
+  :los_angeles => {
     :mayor => "Eric Garcetti",
     :population => 3884307,
     :website => "http://www.lacity.org"
   },
-  :miami => { 
+  :miami => {
     :mayor => "Tomás Regalado",
     :population => 419777,
     :website => "http://www.miamigov.com"
   },
-  :chicago => { 
+  :chicago => {
     :mayor => "Rahm Emanuel",
     :population => 2695598,
     :website => "http://www.cityofchicago.org/"
